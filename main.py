04f874d9169c7b00ef25c6c772365b16d29af82c
xPurplerain <horn.daniel2007@gmail.com>
2021-04-11 22:31:04 +0200

    Initial commit

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import random
 
 # Hier stehen alle Buchstaben die zufällig zusammen gefügt werden
-chars = "abcdefghijklmonopqrstuvwxyzABCDEFGHIJKLMONOPQRSTUVWXYZ1234567890!§$%&/()=?'#-_.;,:-"
+zeichen = "abcdefghijklmonopqrstuvwxyzABCDEFGHIJKLMONOPQRSTUVWXYZ1234567890!§$%&/()=?'#-_.;,:-"
 
 while 1:
     passwd_len = int(input("Wie lang soll dein Passwort sein? "))
-    passwd_count = int(input("Wieviele Passwörter sollen generiert werden? "))
-    for x in range(0, passwd_count):
+    passwd_anzahl = int(input("Wieviele Passwörter sollen generiert werden? "))
+    for x in range(0, passwd_anzahl):
         password = ""
         for x in range(0, passwd_len):
-            password_char = random.choice(chars)
-            password = password + password_char
+            password_zeichen = random.choice(zeichen)
+            password = password + password_zeichen
         print("Fertig -- Hier ist dein zufälliges Passwort: ", password)
