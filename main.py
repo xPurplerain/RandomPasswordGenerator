f3ae1fe20024a7b56b44f08b6d6c0ef23422696f
xPurplerain <horn.daniel2007@gmail.com>
2021-04-11 22:32:18 +0200

    Initial commit

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ import random
 zeichen = "abcdefghijklmonopqrstuvwxyzABCDEFGHIJKLMONOPQRSTUVWXYZ1234567890!§$%&/()=?'#-_.;,:-"
 
 while 1:
-    passwd_len = int(input("Wie lang soll dein Passwort sein? "))
+    passwd_laenge = int(input("Wie lang soll dein Passwort sein? "))
     passwd_anzahl = int(input("Wieviele Passwörter sollen generiert werden? "))
     for x in range(0, passwd_anzahl):
         password = ""
-        for x in range(0, passwd_len):
+        for x in range(0, passwd_laenge):
             password_zeichen = random.choice(zeichen)
             password = password + password_zeichen
         print("Fertig -- Hier ist dein zufälliges Passwort: ", password)
